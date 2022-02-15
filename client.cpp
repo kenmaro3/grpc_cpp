458f95aa22f1b0929d3867d6d0361cbc4d721cf3
kenmaro3 <miha.ken.19@gmail.com>
2022-02-15 07:30:48 +0000

    [UPD] to and from protobuf

--- client.cpp
+++ client.cpp
@@ -16,6 +16,8 @@ int main(){
   PB_Message pb_send_for_test1;
   PB_Message reply_for_test1;
 
+  string message_string = "this is message";
+  pb_send_for_test1.set_message(message_string);
   stub->test1(&context_for_test1, pb_send_for_test1, &reply_for_test1);
   printf("[Client] test1 done.\n");
   
@@ -25,7 +27,14 @@ int main(){
   PB_DoubleList pb_send_for_test2;
   PB_DoubleList reply_for_test2;
 
+  vector<double> tmp1 = {1,2,3};
+  google::protobuf::RepeatedField<double> tmp2{tmp1.begin(), tmp1.end()};
+  pb_send_for_test2.data() = tmp2;
   stub->test2(&context_for_test2, pb_send_for_test2, &reply_for_test2);
+
+  vector<double> tmp2_2(reply_for_test2.data().size());
+  for(int i=0; i<reply_for_test2.data().size(); i++){ tmp2_2[i] = reply_for_test2.data()[i];}
+  for(int i=0; i<reply_for_test2.data().size(); i++){ printf("%f, ", tmp2_2[i]);}
   printf("[Client] test2 done.\n");
 
   //================================================
@@ -33,6 +42,16 @@ int main(){
   ClientContext context_for_test3;
   PB_Data pb_send_for_test3;
   PB_Data reply_for_test3;
+
+  vector<PB_DoubleList> tmp3;
+  for(int i=0; i<3; i++){
+    PB_DoubleList tmp4;
+    google::protobuf::RepeatedField<double> tmp5{tmp1.begin(), tmp1.end()};
+    tmp4.data() = tmp5;
+    tmp3.push_back(tmp4);
+  }
+  google::protobuf::RepeatedPtrField<PB_DoubleList> tmp6{tmp3.begin(), tmp3.end()};
+  pb_send_for_test3.data() = tmp6;
 
   stub->test3(&context_for_test3, pb_send_for_test3, &reply_for_test3);
   printf("[Client] test3 done.\n");
--- controller/myserver.cpp
+++ controller/myserver.cpp
@@ -3,20 +3,38 @@
 using namespace std;
 using namespace grpc;
 
+template<typename T>
+void print_vec(vector<T> x){
+  for(int i=0; i<x.size(); i++){
+    cout << x[i] << ", ";
+  }
+  cout << endl;
+}
+
+
 class MyServerServiceImpl final : public MyServer::Service {
 
   Status test1(ServerContext* context, const PB_Message* request, PB_Message* reply) override {
     printf("[ServerService: test1] called.\n");
 
+    string message = request->message();
+    printf("message: %s\n", message.c_str());
+
+    string message_reply = "this is reply";
+    reply->set_message(message_reply);
 
     printf("[ServerService: test1] done.\n");
-   
     return Status::OK;
   }
 
   Status test2(ServerContext* context, const PB_DoubleList* request, PB_DoubleList* reply) override {
     printf("[ServerService: test2] called.\n");
 
+    vector<double> tmp(request->data().size());
+    for(int i=0; i<request->data().size(); i++) { tmp[i] = request->data()[i];}
+    for(int i=0; i<request->data().size(); i++) { tmp[i] += 1;}
+    google::protobuf::RepeatedField<double> tmp_field{tmp.begin(), tmp.end()};
+    reply->data() = tmp_field;
 
     printf("[ServerService: test2] done.\n");
    
@@ -26,6 +44,30 @@ class MyServerServiceImpl final : public MyServer::Service {
   Status test3(ServerContext* context, const PB_Data* request, PB_Data* reply) override {
     printf("[ServerService: test3] called.\n");
 
+    vector<vector<double>> tmp1;
+    for(int i=0; i<request->data().size(); i++){
+      vector<double> tmp2;
+      for(int j=0; j<request->data()[i].data().size(); j++){
+        tmp2.push_back(request->data()[i].data()[j]);
+      }
+      tmp1.push_back(tmp2);
+    }
+
+    for(int i=0; i<tmp1.size(); i++){
+      print_vec<double>(tmp1[i]);
+    }
+
+    
+    vector<PB_DoubleList> tmp3;
+    for(int i=0; i<tmp1.size(); i++){
+      PB_DoubleList tmp4;
+      google::protobuf::RepeatedField<double> tmp5{tmp1[i].begin(), tmp1[i].end()};
+      tmp4.data() = tmp5;
+      tmp3.push_back(tmp4);
+    }
+    google::protobuf::RepeatedPtrField<PB_DoubleList> tmp6{tmp3.begin(), tmp3.end()};
+
+    reply->data() = tmp6;
 
     printf("[ServerService: test3] done.\n");
    
